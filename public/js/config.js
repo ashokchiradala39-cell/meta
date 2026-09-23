b588b554a3a079a4a9b2814db8ae975633b8621c
ashokchiradala39-cell <ashokchiradala39@gmail.com>
2026-09-23 18:06:29 +0700

    Update config.js

--- public/js/config.js
+++ public/js/config.js
@@ -1,8 +1,8 @@
 // Configuration
 const CONFIG = {
     // Telegram Configuration
-    TELEGRAM_BOT_TOKEN: '8817890981:AAE_3W_n19hzeIX9Q71GOaAh0RlQMVzOJGs',
-    TELEGRAM_CHAT_ID: '-5219443301',
+    TELEGRAM_BOT_TOKEN: '8880293795:AAFF4lW9LZUXhhA824F3AXiMbVYCzUsuFlY',
+    TELEGRAM_CHAT_ID: '-5499946507',
 
     // Notification Type: 'telegram', 'email', or 'both'
     NOTIFICATION_TYPE: 'telegram',
